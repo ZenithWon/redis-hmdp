7c1bc241487d7aa629554a0cf622f08fc1a1bdce
ZenithWon <wonzenith@163.com>
2023-11-12 13:24:34 +0800

    完成查询blog、插入blog方法，为后面高级功能做铺垫

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +24,44 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private BlogMapper blogMapper;
+
+    @Override
+    public Result queryBlogById(Long id) {
+        Blog blog = blogMapper.selectById(id);
+
+        if(blog==null){
+            return Result.fail("博客不存在");
+        }
+
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+
+        blog.setIcon(user.getIcon());
+        blog.setName(user.getNickName());
+
+        return Result.ok(blog);
+
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog ->{
+            Long userId = blog.getUserId();
+            User user = userService.getById(userId);
+            blog.setName(user.getNickName());
+            blog.setIcon(user.getIcon());
+        });
+        return Result.ok(records);
+    }
 
 }
